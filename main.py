e9e67e1f572640059cc351c63fe67eeac4baaf31
erikbl <erik.blokland@adyen.com>
2023-02-02 10:38:43 +0100

    vaguely functional app profiling

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 # This is a sample Python script.
+import types
+
 from utils import AdbHelper
 from configparser import ConfigParser
 from Intent import Intent, Actions, ExtraTypes, Extra
+from app_profiler import AppProfiler
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 def simpleperfCmdBuilder(config: ConfigParser):
+    newcfg = types.SimpleNamespace()
     str = ''
     cfg = config['SIMPLEPERF']
-    loc = cfg.get('simpleperfdevicelocation')
-    if loc is None:
-        str += 'simpleperf'
-    else:
-        str += loc
-    str += ' record '
+    #loc = cfg.get('simpleperfdevicelocation')
+    #if loc is None:
+    #    str += 'simpleperf'
+    #else:
+    #    str += loc
+    #str += ' record '
     rawstr = cfg.get('raw')
     if rawstr is not None:
         return str + rawstr
@@ -23,7 +27,8 @@ def simpleperfCmdBuilder(config: ConfigParser):
     if aut is None:
         raise Exception('unknown aut, can\'t proceed')
 
-    str += ' --app ' + aut
+    #str += ' --app ' + aut
+    newcfg.app = aut
 
     events = cfg.get('events')
     if events is not None and events != '':
@@ -49,15 +54,25 @@ def simpleperfCmdBuilder(config: ConfigParser):
 
     outfile = cfg.get('simpleperfoutputpath')
     if outfile is not None:
-        str += ' -o ' + outfile
+        newcfg.perf_data_path = outfile
 
-    return str
+    newcfg.record_options = str
+    newcfg.disable_adb_root = True
+    newcfg.native_lib_dir = None
+    newcfg.compile_java_code = None
+    newcfg.activity = None
+    newcfg.test = None
+    newcfg.log = 'info'
+    newcfg.skip_collect_binaries = True
+    return newcfg
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     cfg = ConfigParser()
     cfg.read('config.ini')
-    print(simpleperfCmdBuilder(cfg))
+    args = simpleperfCmdBuilder(cfg)
+    profiler = AppProfiler(args)
+    profiler.profile()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
